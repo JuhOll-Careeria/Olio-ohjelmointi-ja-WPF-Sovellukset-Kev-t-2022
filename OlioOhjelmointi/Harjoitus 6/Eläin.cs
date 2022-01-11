ec779806db963b92b23a06f3da885d2a1587cc19
Olli Juhola <juhola.olli@gmail.com>
2022-01-11 12:56:35 +0200

    Harjoitus 6

---
+++ OlioOhjelmointi/Harjoitus 6/Eläin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harjoitus_6
+{
+    abstract class Eläin // <-- Abstrakti luokka
+    {
+        private static int instanssit = 0; // instassi laskuri, eli kuinka monta eläintä on olemassa
+
+        public Eläin()
+        {
+            instanssit++; // aina kun uusi eläin luodaan, lisätään instasseihin yksi lisää
+        }
+
+        public static void KuinkaMonta()
+        {
+            Console.WriteLine("Eläimiä on elossa: " + instanssit);
+        }
+
+        public abstract void Ääntele(); // Abstrakti metodi, eli pakotetaan aliluokat toteuttamaan tämä toiminto
+    }
+}
---
+++ OlioOhjelmointi/Harjoitus 6/Kissa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harjoitus_6
+{
+    class Kissa : Eläin
+    {
+        public int Ikä;
+        public string Väri;
+
+        public static int instanssit = 0;
+
+        public Kissa(string väri, int ikä)
+        {
+            Väri = väri;
+            Ikä = ikä;
+
+            instanssit++;
+        }
+
+        public static new void KuinkaMonta() // new -avain sanalla korvataan Eläin luokan "KuinkaMonta" toiminto kokonaan
+        {
+            Console.WriteLine("Kissoja on elossa: " + instanssit);
+        }
+
+        public override void Ääntele()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+}
