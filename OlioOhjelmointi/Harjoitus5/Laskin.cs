dcf123170be557f2f1fbdcc976ef82d0195bad03
Olli Juhola <juhola.olli@gmail.com>
2022-01-11 10:13:10 +0200

    Harjoitus 5 + Staattisuus

---
+++ OlioOhjelmointi/Harjoitus5/Laskin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harjoitus5
+{
+    // staattinen luokka, merkitään "static" avainsanalla ennen "class" määritettä
+    static class Laskin
+    {
+        // Static metodi, joka palauttaa "float" arvon returnilla
+        public static float Summa(float a, float b)
+        {
+            return a + b;
+        }
+
+        public static float Erotus(float a, float b)
+        {
+            return a - b;
+        }
+
+        public static float Kertolasku(float a, float b)
+        {
+            return a * b;
+        }
+
+        public static float Jakolasku(float a, float b)
+        {
+            return a / b;
+        }
+
+
+    }
+}
---
+++ OlioOhjelmointi/Static Esimerkki/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Static_Esimerkki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float km = 154f; // 154 "kilometriä"
+            // Muutetaan kilometrit maileiksi kutsumalla staattista Muuntaja -luokkaa
+            float mailit = Muuntaja.KmToMiles(km); // <-- Tallennetaan muunnettu arvo "mailit" muuttujaan
+
+            Console.WriteLine(km + " kilometriä on yhtä kuin " + mailit + " mailia");
+
+        }
+    }
+}
