a21faf43fcc48454398ef0a20fade26c689cbd72
Olli Juhola <juhola.olli@gmail.com>
2022-01-11 10:53:28 +0200

    Perintä esimerkki

---
+++ OlioOhjelmointi/Perintä/Opiskelija.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Perintä
+{
+    class Opiskelija : Henkilö
+    {
+        public int opiskelijaID;
+        public string koulu;
+
+        public Opiskelija(string nimi, int ikä, int henkilötunnus, int _opiskelijaID, string _koulu) : base(nimi, ikä, henkilötunnus)
+        {
+            opiskelijaID = _opiskelijaID;
+            koulu = _koulu;
+        }
+
+        // override ylilukee Henkilö -luokan "TulostaTiedot()" metodin
+        public override void TulostaTiedot()
+        {
+            Console.WriteLine("Opiskelija: " + Nimi + ". Ikä: " + Ikä + ". Koulu: " + koulu + ". OpiskelijaID: " + opiskelijaID);
+        }
+
+        public void Opiskele()
+        {
+            Console.WriteLine(Nimi + " Opiskelee...");
+        }
+    }
+}
---
+++ OlioOhjelmointi/Perintä/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Perintä
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Henkilö henkilö = new Henkilö("Matti", 20, 1042104); // Henkilö olio. Henkilö EI ole Opiskelija
+            Opiskelija opiskelija = new Opiskelija("Pekka", 18, 2155821, 12345, "Careeria"); // Opiskelija olio. Opiskelija on Henkilö
+            
+            henkilö.TulostaTiedot();
+            opiskelija.TulostaTiedot();
+
+            henkilö.Syö();
+            henkilö.Juo();
+
+            Console.WriteLine();
+
+            opiskelija.Opiskele();
+            opiskelija.Juo();
+            opiskelija.Syö();
+            opiskelija.Opiskele();
+        }
+    }
+}
